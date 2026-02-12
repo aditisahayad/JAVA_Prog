ad64a8f89b941683810636f5085f447a1dfcfb0d
Aditi Sahay <133731264+aditisahayad@users.noreply.github.com>
2026-02-12 12:45:48 +0000

    Incomplete Prog...

---
+++ Week3_prog4.java
@@ -0,0 +1,13 @@
+/*A Student class with private fields (name, age) is provided, Your task is to make the following: 
+- a parameterized constructor to initialize the private fields
+- the getter/setter methods for each field
+Follow the naming convention as given in the main method of the suffix code.*/
+
+import java.util.*;
+
+class Week3_prog4{  //Instead of naming class as Student, I have named it as Week3_prog4 as I am keeping records of my program s
+    private String name;
+    private int age;
+
+    
+}
